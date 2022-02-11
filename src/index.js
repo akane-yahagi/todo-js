b9a337b3ef99376fdfe88394b59d7515f239e027
akane-yahagi <73518991+akane-yahagi@users.noreply.github.com>
2022-02-11 14:33:38 +0900

    BACKボタン処理追加

--- src/index.js
+++ src/index.js
@@ -4,20 +4,36 @@ const addClick = () => {
   const inputText = document.getElementById("add_text").value;
   document.getElementById("add_text").value = null;
 
+  createIncompleteList(inputText);
+};
+
+// 指定のリストから指定の要素を削除
+const removeFromList = (target, child) => {
+  document.getElementById(target).removeChild(child);
+};
+
+// 未完了リストに指定の要素を格納
+const storeToIncompleteList = (child) => {
+  const ul = document.getElementById("incomplete_list");
+  ul.appendChild(child);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // divタグ生成
   const div = document.createElement("div");
   div.className = "list_row";
 
   // liタグ生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // btn(DONE)タグ生成
   const doneBtn = document.createElement("button");
   doneBtn.innerText = "DONE";
   doneBtn.addEventListener("click", () => {
     // 未完了から削除
-    removeFromIncompleteList(doneBtn.parentNode);
+    removeFromList("incomplete_list", doneBtn.parentNode);
 
     // 親div要素取得
     const addTarget = doneBtn.parentNode;
@@ -33,6 +49,14 @@ const addClick = () => {
     const backBtn = document.createElement("button");
     backBtn.innerText = "Back";
 
+    backBtn.addEventListener("click", () => {
+      const removeTag = backBtn.parentNode;
+      removeFromList("complete_list", removeTag);
+
+      const tagetText = removeTag.firstChild.innerText;
+      createIncompleteList(tagetText);
+    });
+
     // 親divに格納
     addTarget.appendChild(completedli);
     addTarget.appendChild(backBtn);
@@ -46,7 +70,7 @@ const addClick = () => {
   deleteBtn.innerText = "DELETE";
   deleteBtn.addEventListener("click", () => {
     // 親のdivタグをincomplete_listから削除
-    removeFromIncompleteList(deleteBtn.parentNode);
+    removeFromList("incomplete_list", deleteBtn.parentNode);
   });
   // divタグの子要素に各要素を設置
   div.appendChild(li);
@@ -54,13 +78,7 @@ const addClick = () => {
   div.appendChild(deleteBtn);
 
   // 用意しているulタグの子要素にdivタグ設置
-  const ul = document.getElementById("incomplete_list");
-  ul.appendChild(div);
-};
-
-// 未完了リストから指定の要素を削除
-const removeFromIncompleteList = (child) => {
-  document.getElementById("incomplete_list").removeChild(child);
+  storeToIncompleteList(div);
 };
 
 document.getElementById("add_btn").addEventListener("click", () => addClick());
